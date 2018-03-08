5792e8f9a55d93bca00ca2e2ec0e9ddf77d382c3
michael hohl <mhohl@rocketmail.com>
2018-03-08 19:13:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return array.unshift(element)
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
